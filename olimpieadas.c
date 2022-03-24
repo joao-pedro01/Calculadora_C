9d9a9611d77f52bdbc7afd4f8b1e43a7ae3b5648
joao-pedro01 <joao_pedro01@terra.com.br>
2022-03-24 15:31:53 -0300

    Add function LancarPontos

--- olimpieadas.c
+++ olimpieadas.c
@@ -3,21 +3,58 @@
 #include <stdlib.h>
 #include <ctype.h>
 void menu();
+void LacarPontos();
 
 int main()
 {
     setlocale(LC_ALL, "portuguese");
 
+    int Op, QntPaises, i;
+    
     menu();
+
+    Op = getch();
+    towlower(Op);
+
+    switch (Op)
+    {
+    case 'p':
+        printf("Quantos países estão participando?");
+        scanf("%i", &QntPaises);
+        for(i=0; i < QntPaises; i++)
+        {
+            LancarPontos();
+        }
+        break;
+    
+    default:
+        break;
+    }
 
     return 0;
 }
 void menu()
 {
     system("cls");
-    printf("*=============================================*\n");
-    printf("| P - Total de pontos do país                 |\n");
-    printf("| G - Fínal do campeonato de Ginástica solo   |\n");
-    printf("| S - Sair                                    |\n");
-    printf("*=============================================*\n");
+    printf("*===============================================*\n");
+    printf("|   P - Total de pontos do país                 |\n");
+    printf("|   G - Fínal do campeonato de Ginástica solo   |\n");
+    printf("|   S - Sair                                    |\n");
+    printf("|   Digite uma opção:___                        |\n");
+    printf("*===============================================*\n");
 }//end menu
+void LancarPontos()
+{
+   int Ouro, Prata, Bronze, Total;
+
+    printf("\n\nQuantas medalhas de Ouro o país recebeu?");
+    scanf("%i", &Ouro);
+    printf("Quantas medalhas de Prata o país recebeu?");
+    scanf("%i", &Prata);
+    printf("Quantas medalhas de Bronze o país recebeu?");
+    scanf("%i", &Bronze);
+
+    Total = ((Ouro * 3) + (Prata * 2) + (Bronze * 1));
+
+    printf("\n\nPaís %i recebeu %i pontos\n", Total);
+}
