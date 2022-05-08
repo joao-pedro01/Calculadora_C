95fa6763664bad4dd2fdfd5d380c7209f9bfcdfa
joao-pedro01 <joao_pedro01@terra.com.br>
2022-05-08 17:33:29 -0300

    Add game memory

---
+++ 2semestre/challengeDesafio/memory.c
@@ -0,0 +1,15 @@
+/* 
+    Faça um programa que receba 2 notas do aluno. Crie uma função para consistir essas notas (somente aceite nota entre 0 e 10). Crie uma função para calcular a média e outra função para verificar a situação do aluno. Se o aluno tiver média menor que 3, deve aparecer a mensagem ?Reprovado?, senão, se a média for entre 3 e 7, deve aparecer a mensagem ?Aluno em Exame?. Para os que tiverem média maior ou igual a 7 deve aparecer ?Aprovado?.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <ctype.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+
+    return 0;
+}
