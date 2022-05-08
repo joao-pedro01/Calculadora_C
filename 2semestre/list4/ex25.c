7d6325d90402e5349d2791bac5efeb66821411f5
joao-pedro01 <joao_pedro01@terra.com.br>
2022-05-08 15:38:19 -0300

    Add file ex25.c

---
+++ 2semestre/list4/ex25.c
@@ -0,0 +1,18 @@
+/*
+    Faça um programa que leia 3 nomes(matriz) com suas respectivas idades (vetor) e depois mostre qual o nome da pessoa que tem a maior idade. 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+
+
+
+
+
+    return 0;
+}
