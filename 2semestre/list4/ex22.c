e31ba08c33234703dd86161e5be6482f7debd38e
joao-pedro01 <joao_pedro01@terra.com.br>
2022-05-08 22:33:58 -0300

    add ex22.c

---
+++ 2semestre/list4/ex22.c
@@ -0,0 +1,23 @@
+/*
+    Faça um programa que seja capaz de corrigir a prova teste que foi aplicada aos
+    20 alunos da disciplina de programação.
+    Nessa prova há 5 questões valendo 2 pontos cada.
+    O programa deve receber inicialmente uma lista com as respostas
+    corretas (gabarito), que será uma letra (podendo ser ?a?,?b?,?c?,?d?). 
+    Depois o programa irá receber as respostas de cada aluno e mostrará
+    sua respectiva nota (exemplo: se o aluno acertar 3 questões- cada uma
+    vale 2 pontos- então, ele terá nota 6).
+    Esse programa também mostrará a mensagem ?Precisa estudar um
+    pouco mais!? para os que tiveram nota abaixo de 6. 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <ctype.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+ 
+    return 0;
+}
